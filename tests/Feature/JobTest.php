d25a884e319901d6999a36afdcba15ea7f7c9f15
Petrius Lima <petrius.lima@gmail.com>
2020-04-23 23:59:40 -0300

    Change ExampleTest to JobTest

--- tests/Feature/JobTest.php
+++ tests/Feature/JobTest.php
@@ -9,20 +9,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class JobTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-
     public function testShouldDispatchSingleQueueWhenCalledRoute()
     {
         Queue::fake();
